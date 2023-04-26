7f3d85a0f0089b392c15c32adeddb0a5a2760b8d
Hurjay Medilo <hurjay@hurjay.local>
2023-04-26 11:48:02 +0800

    Initial commit

---
+++ Lab 3/SearchMoviesListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SearchMoviesListViewController.swift
+//  Lab 3
+//
+//  Created by Luis Ligunas on 4/26/23.
+//
+
+import Foundation
+import UIKit
+
+class SearchMoviesListViewController: UITableViewController {
+
+	var queryString = ""
+	private var movieResults = [Movie]()
+	private let movieService = MovieService()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+		title = "Search"
+
+        print("Query string is: ")
+        print(queryString)
+		movieService.searchMovies(query: queryString) { [weak self] response in
+			guard let self = self,
+                  let response = response
+			else { return }
+
+            
+			self.movieResults = response.search
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+
+		}
+        print(movieResults)
+        print("BOO!")
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return movieResults.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = movieResults[indexPath.row].title
+		return cell
+	}
+}
+
